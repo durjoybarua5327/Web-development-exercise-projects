c2d83052f38019aa4aae998fc74090562a61c75e
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-02-21 22:47:34 +0600

    push

--- spotify-clone-2023/script.js
+++ spotify-clone-2023/script.js
@@ -1,19 +1,26 @@
-console.log("Let's start the js")
- 
-async function get_songs(){
-    let a = await fetch("http://127.0.0.1:5500/spotify-clone-2023/music/");
-    let response =await a.text();
-    console.log(response)
-    let div = document.createElement("div");
-    div.innerHTML= response;
-    let as = div.getElementsByTagName("a");
-    let songs=[];
-    for (let i = 0; i < as.length; i++) {
-        const element = as[i];
-        if(element.href.endsWith(".m4a")){
-            songs.push(element.href)
-        }
+console.log("Let's start the js");
+
+async function get_songs() {
+  let a = await fetch("http://127.0.0.1:5500/spotify-clone-2023/music/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".m4a")) {
+      songs.push(element.href);
     }
-    return songs
+  }
+  return songs;
 }
-get_songs()
+
+async function main() {
+  songs = await get_songs();
+  let audio = new Audio(songs[1]);
+//   audio.play();
+  console.log(songs);
+}
+main();
