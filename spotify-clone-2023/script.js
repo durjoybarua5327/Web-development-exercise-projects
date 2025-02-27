79974219d2debf176e94c25d6946e876f7337e96
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-02-28 00:10:36 +0600

    Update script.js

--- spotify-clone-2023/script.js
+++ spotify-clone-2023/script.js
@@ -137,6 +137,7 @@ async function main() {
   });
 
   document.querySelector(".volume").getElementsByTagName("input")[0].addEventListener("change", (e)=>{
+    
     currentsong.volume =parseInt(e.target.value)/100
   })
 }
